41aea277e3bcd53d27eaeb9b13a7f3d6271cf463
Roman zinchuk <zigzag2341@gmail.com>
2019-01-28 15:22:04 +0200

    //

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Medical';
+  deferredPrompt:any;
+
+  constructor() {
+    window.addEventListener('beforeinstallprompt',  (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      this.deferredPrompt = e;
+    
+      this.suggestInstallation();
+    
+    });
+  }
+
+  suggestInstallation(){
+    debugger;
+    if(this.deferredPrompt !== undefined) {
+      // The user has had a postive interaction with our app and Chrome
+      // has tried to prompt previously, so let's show the prompt.
+      this.deferredPrompt.prompt();
+
+      // Follow what the user has done with the prompt.
+      this.deferredPrompt.userChoice.then(function(choiceResult) {
+
+        console.log(choiceResult.outcome);
+
+        if(choiceResult.outcome == 'dismissed') {
+          console.log('User cancelled home screen install');
+        }
+        else {
+          console.log('User added to home screen');
+        }
+
+        // We no longer need the prompt.  Clear it up.
+        this.deferredPrompt = null;
+      });
+    }
+  }
 }
